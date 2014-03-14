10547120196ba3ccc564feff35e8982db1338e52
eugen <eugen@bigbyte.com>
2014-03-14 18:46:13 +0300

    add tst udp server finder

--- 1.c
+++ 1.c
@@ -1,245 +1,68 @@
-/*------------------------------------------------------------------------
- * A demonstration of OpenGL in a  ARGB window 
- *    => support for composited window transparency
- *
- * (c) 2011 by Wolfgang 'datenwolf' Draxinger
- *     See me at comp.graphics.api.opengl and StackOverflow.com
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <string>
 
- * License agreement: This source code is provided "as is". You
- * can use this source code however you want for your own personal
- * use. If you give this source code to anybody else then you must
- * leave this message in it.
- *
- * This program is based on the simplest possible 
- * Linux OpenGL program by FTB (see info below)
+#ifdef _WIN32
+#include <winsock2.h>
+typedef int socklen_t;
+#else
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif
 
-  The simplest possible Linux OpenGL program? Maybe...
-
-  (c) 2002 by FTB. See me in comp.graphics.api.opengl
-
-  --
-  <\___/>
-  / O O \
-  \_____/  FTB.
-
-
-* http://stackoverflow.com/questions/4052940/how-to-make-an-opengl-rendering-context-with-transparent-background
-* g++ ./1.c -lGLU -lGL -lglut -lX11 -lXrender
-* http://www.edwardrosten.com/code/autoconf/
-------------------------------------------------------------------------*/
-
-/*
- * An example of using GtkGLExt in C
- *
- * Written by Davyd Madeley <davyd@madeley.id.au> and made available under a
- * BSD license.
- *
- * This is purely an example, it may eat your cat and you can keep both pieces.
- *
- * Compile with:
- *    gcc -o gtkglext-example `pkg-config --cflags --libs gtk+-2.0 gtkglext-1.0 gtkglext-x11-1.0` gtkglext-example.c
- */
-
-#include <gtk/gtk.h>
-#include <gtk/gtkgl.h>
-#include <GL/gl.h>
-
-#include <math.h>
-
-float boxv[][3] = {
-	{ -0.5, -0.5, -0.5 },
-	{  0.5, -0.5, -0.5 },
-	{  0.5,  0.5, -0.5 },
-	{ -0.5,  0.5, -0.5 },
-	{ -0.5, -0.5,  0.5 },
-	{  0.5, -0.5,  0.5 },
-	{  0.5,  0.5,  0.5 },
-	{ -0.5,  0.5,  0.5 }
-};
-#define ALPHA 0.5
-
-static float ang = 30.;
-
-static gboolean
-expose (GtkWidget *da, GdkEventExpose *event, gpointer user_data)
+bool find_server(std::string * ip)
 {
-	GdkGLContext *glcontext = gtk_widget_get_gl_context (da);
-	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (da);
+	int sockfd,n;
+	struct sockaddr_in addr, server_addr;
+	long bBroadcast = 1;
+	socklen_t rbytes = sizeof(server_addr);
+	sockfd=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
 
-	// g_print (" :: expose\n");
-
-	if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
+	memset(&addr,0x0,sizeof(struct sockaddr_in));
+	memset(&server_addr,0x0,sizeof(struct sockaddr_in));
+	
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr=htonl(INADDR_BROADCAST);
+	addr.sin_port=htons(23162);
+	struct timeval tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,(char*)&tv,sizeof(struct timeval)) < 0)
 	{
-		g_assert_not_reached ();
+		printf("SO_RCVTIMEO %s\n");
+		return false;
 	}
-
-	/* draw in here */
-	        glClearColor(1.0, 0.0, 0.0, 0.5);
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glPushMatrix();
+	if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, (const char *)&bBroadcast, sizeof(bBroadcast)) < 0)
+	{
+		printf("SO_BROADCAST %s\n");
+		return false;
+	}
 	
-	glRotatef (ang, 1, 0, 1);
-	// glRotatef (ang, 0, 1, 0);
-	// glRotatef (ang, 0, 0, 1);
-
-	glShadeModel(GL_FLAT);
-
-
-
-	glBegin (GL_LINES);
-	glColor3f (1., 0., 0.);
-	glVertex3f (0., 0., 0.);
-	glVertex3f (1., 0., 0.);
-	glEnd ();
-	
-	glBegin (GL_LINES);
-	glColor3f (0., 1., 0.);
-	glVertex3f (0., 0., 0.);
-	glVertex3f (0., 1., 0.);
-	glEnd ();
-	
-	glBegin (GL_LINES);
-	glColor3f (0., 0., 1.);
-	glVertex3f (0., 0., 0.);
-	glVertex3f (0., 0., 1.);
-	glEnd ();
-
-	glBegin(GL_LINES);
-	glColor3f (1., 1., 1.);
-	glVertex3fv(boxv[0]);
-	glVertex3fv(boxv[1]);
-	
-	glVertex3fv(boxv[1]);
-	glVertex3fv(boxv[2]);
-	
-	glVertex3fv(boxv[2]);
-	glVertex3fv(boxv[3]);
-	
-	glVertex3fv(boxv[3]);
-	glVertex3fv(boxv[0]);
-	
-	glVertex3fv(boxv[4]);
-	glVertex3fv(boxv[5]);
-	
-	glVertex3fv(boxv[5]);
-	glVertex3fv(boxv[6]);
-	
-	glVertex3fv(boxv[6]);
-	glVertex3fv(boxv[7]);
-	
-	glVertex3fv(boxv[7]);
-	glVertex3fv(boxv[4]);
-	
-	glVertex3fv(boxv[0]);
-	glVertex3fv(boxv[4]);
-	
-	glVertex3fv(boxv[1]);
-	glVertex3fv(boxv[5]);
-	
-	glVertex3fv(boxv[2]);
-	glVertex3fv(boxv[6]);
-	
-	glVertex3fv(boxv[3]);
-	glVertex3fv(boxv[7]);
-	glEnd();
-
-	glPopMatrix ();
-
-	if (gdk_gl_drawable_is_double_buffered (gldrawable))
-		gdk_gl_drawable_swap_buffers (gldrawable);
-
-	else
-		glFlush ();
-
-	gdk_gl_drawable_gl_end (gldrawable);
-
-	return TRUE;
+	char recvline[4];
+	while((n=recvfrom(	sockfd,	recvline, 4, 0, (struct sockaddr *)&server_addr, &rbytes))!=4 || 
+			memcmp(recvline,"ping",4)!=0 )
+	{
+		char sendline = 0;
+		sendto(sockfd,&sendline,1,0,
+					(struct sockaddr *)&addr,sizeof(addr));
+	}
+	*ip =  inet_ntoa(server_addr.sin_addr);
+	return true;
 }
-
-static gboolean
-configure (GtkWidget *da, GdkEventConfigure *event, gpointer user_data)
+int main(int argc, char**argv)
 {
-	GdkGLContext *glcontext = gtk_widget_get_gl_context (da);
-	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (da);
+#ifdef _WIN32
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2,2), &wsaData);
+#endif
 
-	if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
-	{
-		g_assert_not_reached ();
-	}
+	std::string asr;
+	find_server(&asr);
+	printf("IP %s",asr.c_str());
 
-	glLoadIdentity();
-	glViewport (0, 0, da->allocation.width, da->allocation.height);
-	glOrtho (-10,10,-10,10,-20050,10000);
-	glEnable (GL_BLEND);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	glScalef (10., 10., 10.);
-	
-	gdk_gl_drawable_gl_end (gldrawable);
-
-	return TRUE;
-}
-
-static gboolean
-rotate (gpointer user_data)
-{
-	GtkWidget *da = GTK_WIDGET (user_data);
-
-	ang++;
-
-	gdk_window_invalidate_rect (da->window, &da->allocation, FALSE);
-	gdk_window_process_updates (da->window, FALSE);
-
-	return TRUE;
-}
-
-int
-main (int argc, char **argv)
-{
-	GtkWidget *window;
-	GtkWidget *da;
-	GdkGLConfig *glconfig;
-
-	gtk_init (&argc, &argv);
-	gtk_gl_init (&argc, &argv);
-
-	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
-	da = gtk_drawing_area_new ();
-
-	gtk_container_add (GTK_CONTAINER (window), da);
-	g_signal_connect_swapped (window, "destroy",
-			G_CALLBACK (gtk_main_quit), NULL);
-	gtk_widget_set_events (da, GDK_EXPOSURE_MASK);
-
-	gtk_widget_show (window);
-
-	/* prepare GL */
-	glconfig = gdk_gl_config_new_by_mode (
-			GDK_GL_MODE_RGBA |
-			GDK_GL_MODE_DEPTH |
-			GDK_GL_MODE_DOUBLE);
-
-	if (!glconfig)
-	{
-		g_assert_not_reached ();
-	}
-
-	if (!gtk_widget_set_gl_capability (da, glconfig, NULL, TRUE,
-				GDK_GL_RGBA_TYPE))
-	{
-		g_assert_not_reached ();
-	}
-
-	g_signal_connect (da, "configure-event",
-			G_CALLBACK (configure), NULL);
-	g_signal_connect (da, "expose-event",
-			G_CALLBACK (expose), NULL);
-
-	gtk_widget_show_all (window);
-
-	g_timeout_add (1000 / 60, rotate, da);
-
-	gtk_main ();
+#ifdef _WIN32
+	WSACleanup();
+#endif
 }
